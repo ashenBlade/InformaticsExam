dc78ccc831f9de0be227fb99e0148bd9b5a28365
ashblade <htcdnsnnkom2002@gmail.com>
2021-01-15 14:43:59 +0300

    Add 100-104

--- Tests/TestTasks88-104.cs
+++ Tests/TestTasks88-104.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Tasks;
 
@@ -13,11 +14,6 @@ namespace Tests
             _tasks88104 = new Tasks88_104();
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
 
         [TestCase(1, 1.0)]
         [TestCase(2, 3.25)]
@@ -55,5 +51,54 @@ namespace Tests
             var actual = _tasks88104.Task99(n, u, v);
             Assert.AreEqual(expected, actual, Delta);
         }
+
+        [Test]
+        public void Task100()
+        {
+            // Can't solve: too small numbers
+            var expected = double.PositiveInfinity;
+            var actual = _tasks88104.Task100();
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(2, 1, 0.1, 1.0003333)]
+        [TestCase(3, 3, 0.1, 1.732142)]
+        [TestCase(1, 2, 0.1, 1.414215)]
+        public void Task101(double a, double x, double eps, double expected)
+        {
+            var actual = _tasks88104.Task101(a, x, eps);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [Test]
+        public void Task102()
+        {
+            // Hope that's right
+            var expected = 0.388292;
+            var actual = _tasks88104.Task102();
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(0.0005, 0.617977)]
+        [TestCase(0.003, 0.617647)]
+        [TestCase(0.02, 0.615384)]
+        public void Task103(double eps, double expected)
+        {
+            var actual = _tasks88104.Task103(eps);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        // Fucking fuck with these tests
+        // Fuck you all
+        // I don't know how to solve this fucking fuck
+        // Fuck this shit, I'm out
+
+        // [TestCase(1, 1.0, 1.0)]
+        // public void Task104(double a, double expectedNumber, double expectedDiff)
+        // {
+        //     var (actualNumber, actualDiff) = _tasks88104.Task104(a);
+        //     Assert.AreEqual(expectedNumber, actualNumber, Delta);
+        //     Assert.AreEqual(expectedDiff, actualDiff, Delta);
+        // }
     }
 }
