5bf1dd6b78d0be90bfaed2bbfd1e6c0b80a30d4f
Lipatov Alexander <lipatov.work@bk.ru>
2021-01-15 18:43:24 +0300

    Add 111-115

--- Tasks/Tasks110-119.cs
+++ Tasks/Tasks110-119.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tasks
 {
@@ -17,6 +13,87 @@ namespace Tasks
             }
             double result = intermediateResult;
             return result;
+        }
+
+        public double Task111(double x, int n)
+        {
+            double squaredX = x * x;
+            double intermediateResult = n / squaredX;
+
+            while (n > 2)
+            {
+                n /= 2;
+                intermediateResult = n / (squaredX + intermediateResult);
+            }
+            double result = x / (squaredX + intermediateResult);
+            return result;
+        }
+
+        public double Task112(int n, int k)
+        {
+            if (k == 0)
+                return n * (n + 1);
+
+            double intermediateResult = (double)n / k;
+            for (int i = 1; i < k; i++)
+            {
+                intermediateResult = intermediateResult * (n - i) / i;
+            }
+            double result = intermediateResult;
+            return result;
+        }
+
+        public int Task113A(int n)
+        {
+            int product = 1;
+            int firstNum = n % 2 + 2;
+            for (int i = firstNum; i <= n; i += 2)
+            {
+                product *= i;
+            }
+            return product;
+        }
+
+        public int Task113B(int n)
+        {
+            int product = Task113A(n);
+            return n % 2 == 0 ? -product : product;
+        }
+
+        public double Task114A(int n)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1d / (i * i);
+            }
+            return sum;
+        }
+
+        public double Task114C(int n)
+        {
+            double sum = 0;
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+                sum += 1d / fact;
+            }
+            return sum;
+        }
+
+        public double Task115G(int n)
+        {
+            int fact = 1;
+            double sum = 0;
+            double sumOfFractions = 0;
+            for (int k = 1; k <= n; k++)
+            {
+                fact *= k;
+                sumOfFractions += 1d / (k + 1);
+                sum += fact / sumOfFractions;
+            }
+            return sum;
         }
     }
 }
--- Tests/TestTasks110-119.cs
+++ Tests/TestTasks110-119.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Tasks;
 
 namespace Tests
@@ -20,6 +19,63 @@ namespace Tests
         public void Test110(int n, double expectedResult)
         {
             double actual = _tasks110119.Task110(n);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+
+        [TestCase(3, 8, 0.3256386979639476)]
+        [TestCase(3, 2, 0.3253012048192771)]
+        [TestCase(1.5, 16, 0.532817004664049)]
+        public void Test111(double x, int n, double expectedResult)
+        {
+            double actual = _tasks110119.Task111(x, n);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+
+        [TestCase(5, 3, 10)]
+        [TestCase(6, 3, 20)]
+        [TestCase(5, 0, 30)]
+        [TestCase(6, 6, 1)]
+        [TestCase(6, 1, 6)]
+        public void Test112(int n, int k, double expectedResult)
+        {
+            double actual = _tasks110119.Task112(n, k);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+
+        [TestCase(5, 15)]
+        [TestCase(6, 48)]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        public void Test113(int n, int expectedResult)
+        {
+            int actual = _tasks110119.Task113A(n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(5, 1.4636111111111112)]
+        [TestCase(1, 1)]
+        [TestCase(10, 1.5497677311665408)]
+        public void Test114A(int n, double expectedResult)
+        {
+            double actual = _tasks110119.Task114A(n);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+
+        [TestCase(5, 1.7166666666666668)]
+        [TestCase(3, 1.6666666666666665)]
+        [TestCase(1, 1)]
+        public void Test114C(int n, double expectedResult)
+        {
+            double actual = _tasks110119.Task114C(n);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(2, 4.4)]
+        [TestCase(3, 9.938461538461539)]
+        public void Test115G(int n, double expectedResult)
+        {
+            double actual = _tasks110119.Task115G(n);
             Assert.AreEqual(expectedResult, actual, delta);
         }
     }
