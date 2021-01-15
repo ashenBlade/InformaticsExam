3b69f5303ccae7c7abbeb9914a8a933fea045c2a
Lipatov Alexander <lipatov.work@bk.ru>
2021-01-15 10:30:10 +0300

    add 110

---
+++ Tests/TestTasks110-119.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Tasks;
+
+namespace Tests
+{
+    class TestTasks110_119
+    {
+        private Task110_119 _tasks110119;
+        private const double delta = 0.0001;
+        [SetUp]
+        public void SetUp()
+        {
+            _tasks110119 = new Task110_119();
+        }
+
+        [TestCase(5, 0.7619047619)]
+        [TestCase(3, 0.75)]
+        [TestCase(1, 1)]
+        public void Test110(int n, double expectedResult)
+        {
+            double actual = _tasks110119.Task110(n);
+            Assert.AreEqual(expectedResult, actual, delta);
+        }
+    }
+}
