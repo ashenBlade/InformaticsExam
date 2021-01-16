7d7a7f01045c49a21e5d5919f22aeba87f747942
ashblade <htcdnsnnkom2002@gmail.com>
2021-01-16 10:39:11 +0300

    Add 335, 336

--- Tasks/Tasks334-336.cs
+++ Tasks/Tasks334-336.cs
@@ -86,6 +86,154 @@ namespace Tasks
 
             return totalSum;
         }
+
+        public long Task335b(int n)
+        {
+            long sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                long number = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    number *= i;
+                }
+
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        public double Task335c(int n)
+        {
+            double sum = 0;
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                int factorialEnd = i * i;
+                for (int j = (i - 1) * (i - 1) + 1; j <= factorialEnd; j++)
+                {
+                    factorial *= j;
+                }
+
+                sum += 1 / (double) factorial;
+            }
+
+            return sum;
+        }
+
+        public long Task335d(int n)
+        {
+            long sum = 0;
+            long factorial = 1;
+            int coef = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                int factNumber = 2 * i * i + 1;
+                int lastFactNumber = 2 * (i - 1) * (i - 1) + 1;
+                for (int j = lastFactNumber + 1; j <= factNumber; j++)
+                {
+                    factorial *= j;
+                }
+
+                sum += (coef *= -1) * factorial;
+            }
+
+            return sum;
+        }
+
+        public double Task336a(int n, double x)
+        {
+            x = x >= 0
+                    ? x
+                    : -x;
+            double sum = 0;
+            long upperFact = 1;
+            long lowerFact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                upperFact *= (2 * i - 1) * (2 * i);
+                lowerFact = CalculateLowerFactorial(lowerFact, i);
+                sum += (upperFact + x) / (lowerFact);
+            }
+
+            return sum;
+        }
+
+        // i - new index
+        private long CalculateLowerFactorial(long factorial, int i)
+        {
+            int factEnd = i * i;
+            for (int j = (i - 1) * (i - 1) + 1; j <= factEnd; j++)
+            {
+                factorial *= j;
+            }
+
+            return factorial;
+        }
+
+        public double Task336b(int n, double x)
+        {
+            double sum = 0;
+            long innerFact = 1;
+            long outerFact = 1;
+            double power = 1;
+            int coef = 1;
+            double totalFactorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                innerFact *= i;
+                outerFact = CalculateFactorialOfFactorial(innerFact, i) * (innerFact + 1);
+                sum += (coef *= -1) * (power *= x) / outerFact;
+                totalFactorial = innerFact;
+            }
+
+            totalFactorial = 1 / totalFactorial;
+            return sum * totalFactorial;
+        }
+
+        private long CalculateFactorialOfFactorial(long factorial, int i)
+        {
+            var oldFactorial = factorial;
+            for (int j = i + 1; j <= oldFactorial; j++)
+            {
+                factorial *= j;
+            }
+
+            return factorial;
+        }
+
+        public double Task336c(int n, double x)
+        {
+            x *= x;
+            double sum = 0;
+            double power = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                long degree = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    degree *= i;
+                }
+
+                sum += degree * (power *= x);
+            }
+
+            return sum;
+        }
+
+        // Просто охуеть оптимизация
+        // +rep
+        public double Task336d(int n, double x)
+        {
+            double sum = x * n;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (double) (1 + i) / 2;
+            }
+
+            return sum;
+        }
     }
 
 }
--- Tests/TestTasks334-336.cs
+++ Tests/TestTasks334-336.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using NUnit.Framework;
 using Tasks;
 
@@ -13,6 +14,69 @@ namespace Tests
         public void Task335a(int n, int expected)
         {
             var actual = _tasks334.Task335a(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(2, 5)]
+        [TestCase(3, 32)]
+        [TestCase(4, 288)]
+        [TestCase(5, 3413)]
+        public void Task335b(int n, long expected)
+        {
+            var actual = _tasks334.Task335b(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(2, 1.041666666)]
+        [TestCase(3, 1.041669422)]
+        public void Task335c(int n, double expected)
+        {
+            var actual = _tasks334.Task335c(n);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(1, -6)]
+        [TestCase(2, 362874)]
+        [TestCase(3, -121645100408469126)]
+        public void Task335d(int n, long expected)
+        {
+            var actual = _tasks334.Task335d(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(3, 1, 4.043653549)]
+        [TestCase(3, 2, 5.0853229)]
+        [TestCase(3, -2, 5.0853229)]
+        public void Task336a(int n, double x, double expected)
+        {
+            var actual = _tasks334.Task336a(n, x);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(3, 2, -0.055820106)]
+        [TestCase(2, 1, -0.166666666)]
+        public void Task336b(int n, double x, double expected)
+        {
+            var actual = _tasks334.Task336b(n, x);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(3, 2, 1796)]
+        [TestCase(3, 3, 20016)]
+        public void Task336c(int n, double x, double expected)
+        {
+            var actual = _tasks334.Task336c(n, x);
+            Assert.AreEqual(expected, actual, Delta);
+        }
+
+        [TestCase(3, 2, 10.5)]
+        [TestCase(3, 3, 13.5)]
+        [TestCase(4, 4, 23)]
+        public void Task336d(int n, double x, double expected)
+        {
+            var actual = _tasks334.Task336d(n, x);
             Assert.AreEqual(expected, actual);
         }
     }
